d5ddf3cb0d8f526ced5b06be473d2e10afc0021d
asd215038 <asd215038@gmail.com>
2023-11-06 23:51:29 +0800

    practise

--- opencv/prace7.py
+++ opencv/prace7.py
@@ -1,10 +1,9 @@
 import cv2
 
-img = cv2.imread('mimi.jpg')
-img = cv2.resize(img, (0, 0), fx=0.5, fy=0.5)
+img = cv2.imread('qq.jpg')
 gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-faceCascade = cv2.CascadeClassifier('catface_detect.xml')
-faceRect = faceCascade.detectMultiScale(gray, 1.1, 3)
+faceCascade = cv2.CascadeClassifier('face_detect.xml')
+faceRect = faceCascade.detectMultiScale(gray, 1.3, 3)
 print(len(faceRect))
 for (x, y, w, h) in faceRect:
     cv2.rectangle(img, (x, y), (x+w, y+h), (0, 255, 0), 2)
